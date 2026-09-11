98be8f520c9d17efbfb2e7fa45db228b23c99754
Pandu <kottepandurangareddy@gmail.com>
2026-09-11 22:59:30 +0530

    feature selection using 0.95 correlation threshold

--- data_selection.py
+++ data_selection.py
@@ -3,14 +3,28 @@ import pandas as pd
 import pathlib
 from sklearn.feature_selection import VarianceThreshold
 
-def DatasetVarianceThreshold():
+def load_data():
     data = pd.read_parquet(pathlib.Path("data") / "telemetry")
     data["ts"] = pd.to_datetime(data["ts_utc"], utc=True)
+    return data
+
+data = load_data()
+
+def DatasetVarianceThreshold():
     features = data.drop(columns=["ts", "gateway_id", "DateDt", "month"])
     features = features.select_dtypes(include=[np.number])
     selector = VarianceThreshold(threshold=0.01)
     selector.fit(features)
-    return features.columns[selector.get_support()].tolist()
+    return features.columns[selector.get_support()].to_list()
+
+def CorrelationThreshold():
+    cols = DatasetVarianceThreshold()
+    features = data[cols]
+    correlation = features.corr().abs()
+    upper_tri = correlation.where(np.triu(np.ones(correlation.shape), k=1).astype(bool))
+    to_drop = [column for column in upper_tri.columns if any(upper_tri[column] > 0.95)]
+    selected_columns = [column for column in features.columns if column not in to_drop]
+    return selected_columns
 
 def features_selection_validity():
     frame = pd.read_parquet(pathlib.Path("data") / "telemetry")
